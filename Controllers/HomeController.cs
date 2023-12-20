59b1027ffc7a6e3b4b56a02733e795b131e1129b
VictorLima <victoraugustolima1@gmail.com>
2023-12-20 16:11:54 -0300

    Privacy de teste finalizado, lista de perguntas e responder a questao

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using FrontAstro.Models;
+using FrontAstro.Data;
 
 namespace FrontAstro.Controllers;
 
@@ -8,10 +9,14 @@ public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
 
-    public HomeController(ILogger<HomeController> logger)
+    private readonly ApplicationDbContext _context;
+
+    public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
     {
+        _context = context;
         _logger = logger;
     }
+    
 
     public IActionResult Index()
     {
@@ -19,8 +24,10 @@ public class HomeController : Controller
     }
 
     public IActionResult Privacy()
-    {
-        return View();
+    {       
+        var listaPerguntas = _context.Perguntas.ToList();
+
+        return View(listaPerguntas);
     }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
